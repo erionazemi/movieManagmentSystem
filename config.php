cbe19f548f0c0eb12b3d307f4b15009700472e22
erionazemi <167331736+erionazemi@users.noreply.github.com>
2025-05-21 17:09:42 +0200

    .

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+$user="root";
+$pass="";
+$host="localhost";
+$dbname="movie";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+}catch(PDOException $e) {
+    echo "error ". $e->getMessage();
+
+}
+
+
+?>
