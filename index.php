9db7e5471daf9c2d2487512a4e229be96fdfdbe9
erionazemi <167331736+erionazemi@users.noreply.github.com>
2025-06-04 16:40:49 +0200

    rkreukrk

--- index.php
+++ index.php
@@ -51,22 +51,37 @@
 	  border-top-left-radius: 0;
 	  border-top-right-radius: 0;
 	}
+	.form-floating{
+		margin: 10px;
+	}
 	</style>
 </head>
 <body class="text-center">
-<!-- Creating a from which will post some data in loginLogic.php file -->
+<!-- Creating a form which will post us some data in register.php file -->
 <main class="form-signin">
-  <form action="loginLogic.php" method="post">
+  <form action="register.php" method="post">
     <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Register</h1>
 
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Emri" name="emri">
+      <label for="floatingInput">Emri</label>
+    </div>
     <div class="form-floating">
       <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="username">
       <label for="floatingInput">Username</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-      <label for="floatingPassword">Password</label>
+      <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingInput" placeholder="Password" name="password">
+      <label for="floatingInput">Password</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password" name="confirm_password">
+      <label for="floatingPassword">Confirm Password</label>
     </div>
 
     <div class="checkbox mb-3">
@@ -74,8 +89,8 @@
         <input type="checkbox" value="remember-me"> Remember me
       </label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign in</button>
-	<p>Already have an account:<a href="index.php" >Sign up</a> </p>
+    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign up</button>
+    <span>Already have an account: </span><a href="login.php">Sign in</a>
   </form>
 </main>
 
